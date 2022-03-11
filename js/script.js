17d9e21df73409de4ef39e919a854f4b15f12e4b
Marzio95 <marziodellarocca14@gmail.com>
2022-03-11 19:52:48 +0100

    imposto la funzione setInterval

--- js/script.js
+++ js/script.js
@@ -1,58 +1,76 @@
-const app = new Vue ({
-        el: '#root',
-        data: {
-            arrayIndex: 0,
-            arrayFoto:[
-                {
-                    immagine: '01.jpg',
-                    titolo: 'Svezia',
-                    testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                },
-                {
-                    immagine: '02.jpg',
-                    titolo: 'Svizzera',
-                    testo: 'Lorem ipsum',
-                },
-                {
-                    immagine: '03.jpg',
-                    titolo: 'Gran Bretagna',
-                    testo:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                },
-                {
-                    immagine: '04.jpg',
-                    titolo: 'Germania',
-                    testo:   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                },
-                {
-                    immagine: '05.jpg',
-                    titolo: 'Paradise',
-                    testo: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                },
-
-            ],
-        },
-
-        methods: {
-            addIndex(arrayIndex, arrayFoto) {
-                if (arrayIndex < arrayFoto.length - 1){
-                this.arrayIndex++;
-            }else {
-                this.arrayIndex = 0; 
-            }
+const app = new Vue({
+    el: '#root',
+    data: {
+        arrayIndex: 0,
+        arrayFoto: [
+            {
+                immagine: '01.jpg',
+                titolo: 'Svezia',
+                testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
             },
-            removeIndex(arrayIndex, arrayFoto) {
-                if (arrayIndex == 0){
-                    this.arrayIndex = arrayFoto.length - 1;
-                }else {
+            {
+                immagine: '02.jpg',
+                titolo: 'Svizzera',
+                testo: 'Lorem ipsum',
+            },
+            {
+                immagine: '03.jpg',
+                titolo: 'Gran Bretagna',
+                testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                immagine: '04.jpg',
+                titolo: 'Germania',
+                testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                immagine: '05.jpg',
+                titolo: 'Paradise',
+                testo: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+
+        ],
+    },
+
+    methods: {
+        addIndex(arrayIndex, arrayFoto) {
+
+            if (arrayIndex < arrayFoto.length - 1) {
+                this.arrayIndex++;
+            } else {
+                this.arrayIndex = 0;
+            }
+
+        },
+        removeIndex(arrayIndex, arrayFoto) {
+            if (arrayIndex == 0) {
+                this.arrayIndex = arrayFoto.length - 1;
+            } else {
                 this.arrayIndex--;
             }
-            },
-
         },
 
 
 
-    })    
 
-        
-    
+
+
+
+    },
+
+    mounted: function () {
+        this.intervalid1 = setInterval(() => {
+            if (this.arrayIndex < this.arrayFoto.length - 1) {
+                this.arrayIndex++;
+            } else {
+                this.arrayIndex = 0;
+            }
+        }, 3000); 
+    }     
+
+
+
+})
+
+
+
