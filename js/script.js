e92aa97962d6b3a9cec2dbc8608eb9291c23dc2d
Marzio95 <marziodellarocca14@gmail.com>
2022-03-11 16:01:21 +0100

    prerparo data in js e creo thumb con v-for

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const app = new Vue ({
+        el: '#root',
+        data: {
+            arrayFoto:[
+                {
+                    immagine: '01.jpg',
+                    titolo: 'Svezia',
+                    testo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+                },
+                {
+                    immagine: '02.jpg',
+                    titolo: 'Svizzera',
+                    testo: 'Lorem ipsum',
+                },
+                {
+                    immagine: '03.jpg',
+                    titolo: 'Gran Bretagna',
+                    testo:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    immagine: '04.jpg',
+                    titolo: 'Germania',
+                    testo:   'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                },
+                {
+                    immagine: '05.jpg',
+                    titolo: 'Paradise',
+                    testo: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                },
+
+            ],
+        },
+        
+    
+    })
